0d3d021c215a879304ee6d28a5a4669e536d87d4
Andrew Sturges <arsturges@lbl.gov>
2012-04-06 20:38:17 -0400

    moved csv inputs to separate folder

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ import pprint
 import csv
 
 #set up some data objects:
-scenario = 'state_energy_code_compliance_no_increase.csv'
+scenario = 'csv_inputs/state_energy_code_compliance_no_increase.csv'
 code_compliance = convert_csv_to_dictionary_of_dictionaries(scenario)
 
 #this form works because we know it has only 2 columns
 #code_key['13'] == "ASHRAE 2004"
-code_key = dict(csv.reader(open('state_energy_code_key.csv')))
+code_key = dict(csv.reader(open('csv_inputs/state_energy_code_key.csv')))
 
-starts = 'construction_history_by_state.csv'
+starts = 'csv_inputs/construction_history_by_state.csv'
 construction_history_by_state = convert_csv_to_dictionary_of_dictionaries(starts)
 
 #states_cendivs['AZ'] == '8'.
-states_cendivs = dict(csv.reader(open('states_cendivs.csv'))) 
+states_cendivs = dict(csv.reader(open('csv_inputs/states_cendivs.csv'))) 
 
-cendiv_NEMS = import_3_column_data('cendivs_NEMS_percentages.csv')
+cendiv_NEMS = import_3_column_data('csv_inputs/cendivs_NEMS_percentages.csv')
 
 construction_history = add_NEMS_building_types_to_construction_history(
     construction_history_by_state,
