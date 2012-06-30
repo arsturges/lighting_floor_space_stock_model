6356e8eb336f1cdf9161f692275226955300a9ce
Andrew Sturges <arsturges@lbl.gov>
2012-06-29 17:43:17 -0700

    removed debugging code

--- helper_functions.py
+++ helper_functions.py
@@ -17,31 +17,23 @@ def age_building_stock_to_year(building_stock_objects, year):
     return building_stock_objects
 
 def sum_bin_years(stock_objects):
-    pprint.pprint(locals())
     # Combine all individual FloorSpace objects into one.
     # Show a complete picture of all existing floor space bins by state, year,
     # and building type, as it exists in the 'current_year'.
     # (Am I incorrectly assuming all objects will have same current year?)
     # bin_years_sum['CA'][1950][78] = 2467.937 (floor space from all years having a 1950 bin)
-    print("\tinside sum_bin_years")
-    print("\thow many stock objects:",len(stock_objects))
     bin_years_sum = dict() 
     for stock_object in stock_objects:
         state = stock_object.region
         start_year = stock_object.year_of_construction
         end_year = stock_object.current_year
-        print("\tsum function location:",state, start_year, end_year)
         if not state in bin_years_sum:
             bin_years_sum[state] = dict()
-        collector = 0
         for year in range(start_year, end_year + 1):
             if not year in bin_years_sum[state]:
                 bin_years_sum[state][year] = dict()
             for building_type in [1,2,3,4,5,6,9,10,11,78]:
                 if not building_type in bin_years_sum[state][year]:
                     bin_years_sum[state][year][building_type] = 0
-                if end_year == 1904: print(state, start_year, end_year, year, building_type, stock_object.remaining_floor_space_by_year[year][building_type])
-                collector += stock_object.remaining_floor_space_by_year[year][building_type]
                 bin_years_sum[state][year][building_type] += stock_object.remaining_floor_space_by_year[year][building_type]
-        print("\tCollector value:", collector)
     return bin_years_sum
