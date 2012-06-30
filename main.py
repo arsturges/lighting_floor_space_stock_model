2ea3db69bedf13ca55713fed2091b6caaeb1be2a
Andrew Sturges <arsturges@lbl.gov>
2012-06-29 17:43:52 -0700

    DOGGONE DEEPCOPY

--- main.py
+++ main.py
@@ -14,15 +14,6 @@ import cProfile
 
 def main():
 
-    # Load the inputs:
-    inputs = LoadInputs()
-    construction_history_by_state = inputs.construction_history_by_state
-    states_cendivs = inputs.states_cendivs
-    cendiv_NEMS = inputs.cendiv_NEMS
-    construction_history = inputs.construction_history
-    floor_space_coverage_by_code = inputs.floor_space_coverage_by_code
-    code_compliance = inputs.code_compliance
-
     def print_single_floor_space_object(floor_space_object):
         current_year = floor_space_object.current_year
         state = floor_space_object.region
@@ -54,9 +45,11 @@ def main():
                         coverage_multiplier = 0
                     covered_floor_space += bin_years_sum[state][year][building_type] * coverage_multiplier
                     uncovered_floor_space += bin_years_sum[state][year][building_type] * (1 - coverage_multiplier)
-                print(current_year, state, year, uncovered_floor_space)
                 writer.writerow([current_year,state,year,building_type,code_number,code_title,'covered:',covered_floor_space])
                 writer.writerow([current_year,state,year,building_type,code_number,code_title,'uncovered:',uncovered_floor_space])
+
+    # Load the inputs:
+    inputs = LoadInputs()
 
     # Run the model using convenience methods from helper_functions.py:
 
@@ -72,22 +65,21 @@ def main():
                          'floor_space'])
 
     # Create a single FloorSpace object, age it, and print it out:
-##        _1900 = FloorSpace(1900, construction_history['CA'][1900], 'CA')
-##        #pprint.pprint(_1900.remaining_floor_space_by_year)
+##        _1900 = FloorSpace(1900, inputs.construction_history['CA'][1900], 'CA')
 ##        _1900.age_n_years(10)
 ##        print_single_floor_space_object(_1900)
 
     # Create a single snapshot consisting of a list of FloorSpace objects,
     # each aged to the same point in time:
-        _1900_to_1903 = create_building_stock(1900, 1903, construction_history)
-        age_building_stock_to_year(_1900_to_1903, 1903)
+        _1900_to_1903 = create_building_stock(1900, 1903, copy.deepcopy(inputs.construction_history))
+        age_building_stock_to_year(_1900_to_1903, 1903, inputs)
         code_bins = sum_bin_years(_1900_to_1903)
-        print_csv_database_rows(1903, code_bins, code_compliance, floor_space_coverage_by_code)
-        
-        _1900_to_1904 = create_building_stock(1900, 1904, construction_history)
-        age_building_stock_to_year(_1900_to_1904, 1904)
+        print_csv_database_rows(1903, code_bins, inputs.code_compliance, inputs.floor_space_coverage_by_code)
+
+        _1900_to_1904 = create_building_stock(1900, 1904, copy.deepcopy(inputs.construction_history))
+        age_building_stock_to_year(_1900_to_1904, 1904, inputs)
         code_bins = sum_bin_years(_1900_to_1904)
-        print_csv_database_rows(1904, code_bins, code_compliance, floor_space_coverage_by_code)
+        print_csv_database_rows(1904, code_bins, inputs.code_compliance, inputs.floor_space_coverage_by_code)
 
     # Create a range of snapshots
 ##        start_year = 1900
@@ -95,11 +87,11 @@ def main():
 ##        for snapshot_year in range(start_year, end_year + 1):
 ##            start_time_stamp = datetime.now()
 ##            print("Creating a snapshot object spanning", start_year,"--",snapshot_year)
-##            snapshot = create_building_stock(start_year, snapshot_year, construction_history)
+##            snapshot = create_building_stock(start_year, snapshot_year, inputs.construction_history)
 ##            print("Now aging it to", snapshot_year)
 ##            age_building_stock_to_year(snapshot, snapshot_year)
 ##            code_bins = sum_bin_years(snapshot)
-##            print_csv_database_rows(snapshot_year, code_bins, code_compliance, floor_space_coverage_by_code)
+##            print_csv_database_rows(snapshot_year, code_bins, inputs.code_compliance, inputs.floor_space_coverage_by_code)
 ##            end_time_stamp = datetime.now()
 ##            print("Duration between years:", end_time_stamp - start_time_stamp)
 
