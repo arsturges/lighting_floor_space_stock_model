0bdb2d51ca7a999f10dee707f676b7c40c0573e6
Andrew Sturges <arsturges@lbl.gov>
2012-07-02 14:44:22 -0700

    Now only create building stock objects once. Time reduced from 3 hrs to 8 mins.

--- helper_functions.py
+++ helper_functions.py
@@ -16,24 +16,25 @@ def age_building_stock_to_year(building_stock_objects, year):
         building_stock_objects[i].age_n_years(year - start_year)
     return building_stock_objects
 
-def sum_bin_years(stock_objects):
+def sum_bin_years(stock_objects, sum_to_year):
     # Combine all individual FloorSpace objects into one.
     # Show a complete picture of all existing floor space bins by state, year,
-    # and building type, as it exists in the 'current_year'.
-    # (Am I incorrectly assuming all objects will have same current year?)
+    # and building type, as it exists in the 'sum-to year'.
     # bin_years_sum['CA'][1950][78] = 2467.937 (floor space from all years having a 1950 bin)
-    bin_years_sum = dict() 
+    bin_years_sum = dict()
     for stock_object in stock_objects:
-        state = stock_object.region
-        start_year = stock_object.year_of_construction
         end_year = stock_object.current_year
-        if not state in bin_years_sum:
-            bin_years_sum[state] = dict()
-        for year in range(start_year, end_year + 1):
-            if not year in bin_years_sum[state]:
-                bin_years_sum[state][year] = dict()
-            for building_type in [1,2,3,4,5,6,9,10,11,78]:
-                if not building_type in bin_years_sum[state][year]:
-                    bin_years_sum[state][year][building_type] = 0
-                bin_years_sum[state][year][building_type] += stock_object.remaining_floor_space_by_year[year][building_type]
+        if sum_to_year >= end_year:
+            state = stock_object.region
+            start_year = stock_object.year_of_construction
+            end_year = stock_object.current_year
+            if not state in bin_years_sum:
+                bin_years_sum[state] = dict()
+            for year in range(start_year, end_year + 1):
+                if not year in bin_years_sum[state]:
+                    bin_years_sum[state][year] = dict()
+                for building_type in [1,2,3,4,5,6,9,10,11,78]:
+                    if not building_type in bin_years_sum[state][year]:
+                        bin_years_sum[state][year][building_type] = 0
+                    bin_years_sum[state][year][building_type] += stock_object.remaining_floor_space_by_year[year][building_type]
     return bin_years_sum
