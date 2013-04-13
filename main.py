ff5e2cb6e98359956944fb6ef238d904abd3ee76
Andrew Sturges <arsturges@lbl.gov>
2013-04-12 18:01:21 -0700

    clarified function origin

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ start_time = datetime.now()
 
 from floor_space import FloorSpace
 from load_inputs import LoadInputs
-from helper_functions import *
+import helper_functions
+import copy
 import pprint
 import csv
 import cProfile
@@ -19,7 +20,7 @@ def main():
         bin_years_sum):
         for state in bin_years_sum.keys():
             for year in bin_years_sum[state].keys():
-                code_number, code_title = return_code_number_and_title(year, state)
+                code_number, code_title = helper_functions.return_code_number_and_title(year, state)
 ####                sum_across_all_building_types = 0
 ####                for building_type in [1,2,3,4,5,6,9,10,11,78]:
 ####                    sum_across_all_building_types += bin_years_sum[state][year][building_type]
@@ -28,7 +29,7 @@ def main():
                 uncovered_floor_space = 0
                 compliance_rate = 0.25
                 for building_type in [1,2,3,4,5,6,9,10,11,78]:
-                    coverage_multiplier = return_coverage_multiplier(building_type,code_number) * compliance_rate
+                    coverage_multiplier = helper_functions.return_coverage_multiplier(building_type,code_number) * compliance_rate
                     covered_floor_space += bin_years_sum[state][year][building_type] * coverage_multiplier
                     uncovered_floor_space += bin_years_sum[state][year][building_type] * (1 - coverage_multiplier)
                 writer.writerow([current_year,state,year,building_type,code_number,code_title,'covered:',covered_floor_space])
@@ -53,11 +54,11 @@ def main():
     # Create a single, complete building stock, age it one year at a time, then sum and print those years 
         start_year = 1900
         end_year = 2030
-        _1900_to_2030 = create_building_stock(1900, 2030, copy.deepcopy(inputs.construction_history))
+        _1900_to_2030 = helper_functions.create_building_stock(1900, 2030, copy.deepcopy(inputs.construction_history))
         for snapshot_year in range(start_year, end_year + 1):
             print("Aging to year", snapshot_year)
-            age_building_stock_to_year(_1900_to_2030, snapshot_year)
-            code_bins = sum_bin_years(_1900_to_2030, snapshot_year)
+            helper_functions.age_building_stock_to_year(_1900_to_2030, snapshot_year)
+            code_bins = helper_functions.sum_bin_years(_1900_to_2030, snapshot_year)
             print_csv_database_rows(snapshot_year, code_bins)
 main()
 #cProfile.run('main()')
