7c7706a04407cfb173d52c64b20d2d8561465327
Andrew Sturges <arsturges@lbl.gov>
2013-09-10 02:11:02 -0700

    formally test renovation and demolition functions

--- unit_tests.py
+++ unit_tests.py
@@ -31,7 +31,114 @@ class TestRateFunctions(unittest.TestCase):
             self.assertGreaterEqual(rate, 0, "renovation rate should be >= 0")
             self.assertLessEqual(rate, 1, "renovation rate should be <= 1")
 
-"""
+class Renovation(unittest.TestCase):
+    """
+    Check that the correct amount of floor space from each renovation bin is
+    getting moved into the current_year bin.
+    """
+    def setUp(self):
+        self.square_footage = {1:100, 2:100, 3:100, 4:100, 5:100, 6:100, 9:100, 10:100, 11:100, 78:100}
+        self.fs = FloorSpace(1990, self.square_footage, 'CA')
+
+    def test_renovation_shifts_bins_correctly(self):
+        for building_type in [1,2,3,4,5,6,9,10,11,78]:
+            self.assertEqual(self.fs.remaining_floor_space_by_year[1990][building_type], 100)
+        for year in range(1990, 1996):
+            self.fs.current_year +=1
+            self.fs.renovate()
+            for building_type in [1,2,3,4,5,6,9,10,11,78]:
+                self.assertEqual(self.fs.remaining_floor_space_by_year[1990][building_type], 100)
+        # All floor space should have remained in the 1990 bin, because renovation_rate was 0:
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1990].values()), 10*100)
+        self.assertEqual(self.fs.total_remaining_floor_space(), 10*100)
+
+        # now advance it one year, and there should be a positive renovation rate.
+        # current year is 1996 until we advance, so it'll renovate 1990 bins and put them into 1997
+        self.fs.current_year += 1 # so now it's 1997
+        self.fs.renovate() # renovate 1% of 1990 bin, put it into 1997:
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1990].values()), 99*10)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1997].values()), 1*10)
+        # in-between bins should have nothing:
+        for year_bin in range(1991,1997):
+            self.assertEqual(sum(self.fs.remaining_floor_space_by_year[year_bin].values()), 0)
+
+        # Advance another year, 1% of all bins get moved to 1998, total sum should still be 10*100 (self.square_footage)
+        self.fs.current_year += 1 # so now it's 1998
+        self.fs.renovate() # renovate 1% of 1990 bin and 0% of 1997 bin, put it into 1998:
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1990].values()), 10*100*.99*.99)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1997].values()), 1*10)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1998].values()), 10*100*.01*.99)
+        self.assertEqual(self.fs.total_remaining_floor_space(), 10*100)
+
+        # One more year, and I think we can say it's working:
+        self.fs.current_year += 1 # so now it's 1999
+        self.fs.renovate() # renovate 1% of 1990 bin and 0% of 1997 bin and 0% of 1998 bin, put it into 1999:
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1990].values()), 10*100*.99*.99*.99)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1997].values()), 1*10)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1998].values()), 10*100*.01*.99)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1998].values()), 10*100*.01*.99)
+        self.assertEqual(sum(self.fs.remaining_floor_space_by_year[1999].values()), 10*100*.99*.99*.01)
+        self.assertEqual(self.fs.total_remaining_floor_space(), 10*100)
+
+
+class Demolition(unittest.TestCase):
+    """
+    Check that the correct amount of floor space is being removed/demolished
+    from each bin.
+    """
+
+    def setUp(self):
+        self.square_footage = {1:100, 2:100, 3:100, 4:100, 5:100, 6:100, 9:100, 10:100, 11:100, 78:100}
+        self.fs = FloorSpace(1990, self.square_footage.copy(), 'CA')
+
+    def test_demolition(self):
+        self.assertEqual(self.fs.total_remaining_floor_space(), 10*100)
+        self.fs.current_year += 1 # now it's 1991
+        self.fs.demolish()
+
+        building_age = self.fs.current_year - self.fs.year_of_construction
+        self.assertEqual(building_age, 1)
+        for building_type in [1,2,3,4,5,6,9,10,11,78]:
+            surviving_proportion = self.fs.surviving_proportion_wrapper(building_type, building_age)
+            self.assertEqual(self.fs.remaining_floor_space_by_year[1990][building_type], 100*surviving_proportion)
+
+        # Further years require renovate() to create later years' year_bins. Instead we create a simple fixture:
+        fs_later = FloorSpace(1990, self.square_footage.copy(), 'CA')
+        self.assertEqual(fs_later.total_remaining_floor_space(), 10*100)
+        fs_later.current_year += 1 #1991
+        fs_later.remaining_floor_space_by_year[1991]={1:0,2:0,3:0,4:0,5:0,6:0,9:0,10:0,11:0,78:0}
+        self.assertEqual(fs_later.total_remaining_floor_space(), 10*100)
+        fs_later.current_year += 1 #1992
+        fs_later.remaining_floor_space_by_year[1992]={1:5,2:5,3:5,4:5,5:5,6:5,9:5,10:5,11:5,78:5}
+        # So now each bin in 1990 has 100, 1991 has 0, and 1992 has 5. Total sum should 10*100+10*5
+        self.assertEqual(fs_later.current_year, 1992)
+        self.assertEqual(fs_later.total_remaining_floor_space(), 10*100 + 10*5)
+
+        # now demo it in 1993, and see that the right amount of floor space from each of 1990, 1991 (which is 0), and 1992 (5 each) 
+        # got demolished:
+
+        fs_later.current_year +=1 # now 1993
+        fs_later.demolish()
+        #1990 bin is 3 years old
+        building_age = fs_later.current_year - 1990
+        for building_type in [1,2,3,4,5,6,9,10,11,78]:
+            surviving_proportion =fs_later.surviving_proportion_wrapper(building_type, building_age)
+            self.assertEqual(fs_later.remaining_floor_space_by_year[1990][building_type], 100*surviving_proportion)
+
+        #1991 bin is 2 years old
+        building_age = fs_later.current_year - 1991
+        for building_type in [1,2,3,4,5,6,9,10,11,78]:
+            surviving_proportion =fs_later.surviving_proportion_wrapper(building_type, building_age)
+            self.assertEqual(fs_later.remaining_floor_space_by_year[1991][building_type], 0*surviving_proportion)
+
+        #1992 bin is 1 years old
+        building_age = fs_later.current_year - 1992
+        for building_type in [1,2,3,4,5,6,9,10,11,78]:
+            surviving_proportion =fs_later.surviving_proportion_wrapper(building_type, building_age)
+            self.assertEqual(fs_later.remaining_floor_space_by_year[1992][building_type], 5*surviving_proportion)
+
+
+    
 class TestDistributeToNewBinYear(unittest.TestCase):
     # Receive the newly renovated floor space, the floor space object consisting
     # of the remaining floor space that wasn't renovated, and the year in which
@@ -45,10 +152,8 @@ class TestDistributeToNewBinYear(unittest.TestCase):
         
     def test_bin_year_is_next_greatest_year(self):
         self.fs.age_n_years(10) # now it has 10 bin_years
-        pprint.pprint(self.test_object)
 
     # New object should have one more bin than old object
-"""
 
 class TestCombiningBinYears(unittest.TestCase):
     def setUp(self):
